2037e5c68b98855c88c40c9bcd48d23762fb1dec
Aman-singh15 <aman15dece@gmail.com>
2024-10-16 22:44:18 +0530

    Update majority_Element.java

--- majority_Element.java
+++ majority_Element.java
@@ -1,27 +1,26 @@
 // Given an array nums of size n, return the majority element.
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
-   import java.util.HashMap;
 
+import java.util.HashMap;
 public class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
-        int majorityElement = nums[0];
-        int maxCount = 0;
-
-        // Step 1: Iterate through the array and populate the HashMap
-        for (int num : nums) {
-            // Increment the count for the current number
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-
-            // Step 2: Track the element with the highest frequency
-            if (frequencyMap.get(num) > maxCount) {
-                maxCount = frequencyMap.get(num);
-                majorityElement = num;  // Update the majority element
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
             }
         }
-
-        // Step 3: Return the element with the highest frequency
-        return majorityElement;
+        int max=0;
+        int maxkey=0;
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i])>max){
+                max=map.get(nums[i]);
+                maxkey=nums[i];
+            }
+        }
+        return maxkey;
     }
 }
  
