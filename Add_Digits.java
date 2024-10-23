d381e4bf6701ffe43916bedb9e68cc03d55a6c69
Aman-singh15 <aman15dece@gmail.com>
2024-10-23 20:40:49 +0530

    Update Add_Digits.java

--- Add_Digits.java
+++ Add_Digits.java
@@ -1,4 +1,10 @@
 //Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
 class Solution {
     public int addDigits(int num) {
         if(num%10==num){
