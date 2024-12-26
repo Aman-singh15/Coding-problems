45e7131d860860b0857915b35a97051174a87a48
Aman-singh15 <aman15dece@gmail.com>
2024-12-26 22:23:37 +0530

    Create Check_If_Array_is_Good.java

---
+++ Check_If_Array_is_Good.java
@@ -0,0 +1,26 @@
+// You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+// base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1 exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+// Return true if the given array is good, otherwise return false.
+// Note: A permutation of integers represents an arrangement of these numbers.
+// Example 1:
+// Input: nums = [2, 1, 3]
+// Output: false
+// Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a permutation of base[n], is n = 3. However, base[3] has four elements but array nums has three. Therefore, it can not be a permutation of base[3] = [1, 2, 3, 3]. So the answer is false.
+
+import java.util.Arrays;
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        // Step 2: Check if the array contains 1 to n-1 and n-1 twice
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+
+        // Check if the last element is equal to n-1
+        return nums[n - 1] == n - 1;
+    }
+}
