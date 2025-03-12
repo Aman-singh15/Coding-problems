d687055ac03f5bb90827210eef2834afb4bde826
Aman-singh15 <aman15dece@gmail.com>
2025-03-12 19:39:35 +0530

    Create Minimum_Element_In_BST.java

---
+++ Minimum_Element_In_BST.java
@@ -0,0 +1,40 @@
+/*Given the root of a Binary Search Tree. The task is to find the minimum valued element in this given BST.
+Examples
+Input: root = [5, 4, 6, 3, N, N, 7, 1]
+ex-1
+Output: 1
+Input: root = [10, 5, 20, 2]
+ex-2
+Output: 2
+Input: root = [10, N, 10, N, 11]
+             10
+              \
+               10
+                \
+                 11
+Output: 10
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        if(root==null){
+            return 0;
+        }
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root.data;
+    }
+}
